f20c61cfff02dcd8a010c0972cb03eb3cb6dc067
Matthew Genova <mgenova@anna-elliss-macbook-pro.local>
2016-08-02 21:19:17 -0500

    Added better start button

--- Redead/MenuScene.swift
+++ Redead/MenuScene.swift
@@ -21,13 +21,18 @@ class MenuScene: SKScene {
         let x = ScreenHelper.instance.visibleScreen.origin.x
         let y = ScreenHelper.instance.visibleScreen.origin.y
 
-        let buttonSize = CGSize(width: screenWidth/10, height: screenWidth/10)
+        let buttonSize = CGSize(width: 500, height: 200)
         
-        let zButton = SgButton(normalImageNamed: "Assets/blueButton.png", highlightedImageNamed: "Assets/bluePushed.png", buttonFunc: tappedStartButton)
+        let zButton = SgButton(normalImageNamed: "Assets/buttonStock.png", highlightedImageNamed: "Assets/buttonStockPressed.png", buttonFunc: tappedStartButton)
         zButton.size = buttonSize
         zButton.position = CGPointMake(x + screenWidth / 2, y + screenHeight / 2)
         
         self.addChild(zButton)
+        let label = SKLabelNode(text: "Start Redead")
+        label.fontName = "AmericanTypewriter-Bold"
+        label.fontColor = UIColor.yellowColor()
+        label.zPosition = 0.1
+        zButton.addChild(label)
     }
     
     func tappedStartButton(button: SgButton){
